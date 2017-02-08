154cd7600b0f588253f176cffaab3f89b9397167
jbfues <jbfues@uchicago.edu>
2017-02-08 14:21:06 -0600

    Create util.py

---
+++ util.py
@@ -0,0 +1,19 @@
+import numpy as np
+import string
+
+def break_string(input_str):
+    '''
+    Takes a string and returns a list of words in the string.
+    Inputs:
+        One String
+    Outputs:
+        A list of words in the string
+    '''
+    raw_string = input_str.lower()
+    string_list = raw_string.split()
+    punc_list = list(string.punctuation)
+    for item in string_list:
+        for punc in punc_list:
+            item = item.replace(punc, '')
+    return string_list
+        
