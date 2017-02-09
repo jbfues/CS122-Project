6a81d524094cce847ca8784dabf89041179040ee
Anne Rogers <amr@cs.uchicago.edu>
2017-02-08 19:50:59 -0600

    Added function to retrieve inspection data from the city's data portal and updated docstring of get_api_key() in util.py

--- util.py
+++ util.py
@@ -1,8 +1,43 @@
 import numpy as np
 import string
 import json
+import requests
+
+CITY_API_ENDPOINT = 'https://data.cityofchicago.org/resource/cwig-ma7x.json'
+
+def get_inspections(min_date = None):
+    '''
+    Retrieves all available food inspection data from the city data portal
+
+    Input: min_date (string) in the form of a floating timestamp
+
+            https://dev.socrata.com/docs/datatypes/floating_timestamp.html
+
+    Output: list of dicts, where each dict represents an inspection
+    '''
+    req = CITY_API_ENDPOINT
+    if min_date != None:
+        # concatonate a query string to the request using SoQL
+        # https://dev.socrata.com/docs/queries/
+        req += '?$where=inspection_date > ' + min_date
+    resp = requests.get(req)
+    return resp.json()
+
 
 def get_api_key(filename = 'yelp_api_key.json'):
+    '''
+    Retrieves API v2.0 key stored in a json file in the local directory
+
+    Input: .json filename
+
+    Output: dictionary in the following form
+        {
+            "Consumer Key": <string>,
+            "Consumer Secret": <string>,
+            "Token": <string>,
+            "Token Secret": <string>
+        }
+    '''
     json_file = open(filename)
     json_str = json_file.read()
     return json.loads(json_str)
@@ -28,4 +63,4 @@ def break_string(input_str):
     return final_list
 
 
-        
+
