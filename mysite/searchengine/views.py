0d9f447b217d1be1aa8ed4594c4f5511a35c843b
Anne Rogers <amr@cs.uchicago.edu>
2017-03-05 23:26:28 -0600

    Started django UI. All the code is done except for that to interect with the database -- i.e. all the views are complete sans data

---
+++ mysite/searchengine/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import request
+from mysite.searchengine.restaurant_search import search_rests
+
+def search(request):
+    if request.POST:
+        term = request.POST['term']
+        render(request, 'search.html', {'result': search_rests(term, 10)})
+    else:
+        return render(request, 'search.html')
