58545fd29a371c7fa4485532bc0e991e377090b5
Anne Rogers <amr@cs.uchicago.edu>
2017-03-07 20:05:14 -0600

    Added comment header to make_email_list.py

--- make_email_list.py
+++ make_email_list.py
@@ -1,3 +1,8 @@
+#A script to make email_list table in db.sql
+# run the following command to use:
+#
+#   python make_email_list.py
+
 import sqlite3
 
 db_file = '/home/student/CS122-Project/db.sql'
